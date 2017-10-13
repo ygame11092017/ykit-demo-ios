f3cfef8aebecb7a80d0d521c2f3074504db7d85b
quan <quan1910@hotmail.com>
2017-10-13 14:56:21 +0700

    Example project

---
+++ proj.ios_mac/ios/AppController.mm
@@ -0,0 +1,346 @@
+/****************************************************************************
+ Copyright (c) 2010-2013 cocos2d-x.org
+ Copyright (c) 2013-2017 Chukong Technologies Inc.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#import "AppController.h"
+#import "cocos2d.h"
+#import "AppDelegate.h"
+#import "RootViewController.h"
+#import <YKit/Ykit.h>
+
+
+// Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
+// running iOS 10 and above.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+#import <UserNotifications/UserNotifications.h>
+@interface AppController () <UNUserNotificationCenterDelegate>
+@end
+#endif
+
+// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
+#ifndef NSFoundationVersionNumber_iOS_9_x_Max
+#define NSFoundationVersionNumber_iOS_9_x_Max 1299
+#endif
+
+#define SYSTEM_VERSION_GRATERTHAN_OR_EQUALTO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
+
+@implementation AppController
+
+@synthesize window;
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+// cocos2d application instance
+static AppDelegate s_sharedApplication;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    cocos2d::Application *app = cocos2d::Application::getInstance();
+    
+    // Initialize the GLView attributes
+    app->initGLContextAttrs();
+    cocos2d::GLViewImpl::convertAttrs();
+    
+    // Override point for customization after application launch.
+
+    // Add the view controller's view to the window and display.
+    window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+
+    // Use RootViewController to manage CCEAGLView
+    _viewController = [[RootViewController alloc]init];
+    _viewController.wantsFullScreenLayout = YES;
+    
+
+    // Set RootViewController to window
+    if ( [[UIDevice currentDevice].systemVersion floatValue] < 6.0)
+    {
+        // warning: addSubView doesn't work on iOS6
+        [window addSubview: _viewController.view];
+    }
+    else
+    {
+        // use this method on ios6
+        [window setRootViewController:_viewController];
+    }
+
+    [window makeKeyAndVisible];
+
+    [[UIApplication sharedApplication] setStatusBarHidden:true];
+    
+    // IMPORTANT: Setting the GLView should be done after creating the RootViewController
+    cocos2d::GLView *glview = cocos2d::GLViewImpl::createWithEAGLView((__bridge void *)_viewController.view);
+    cocos2d::Director::getInstance()->setOpenGLView(glview);
+    
+    //run the cocos2d-x game scene
+    app->run();
+    
+    
+    // Start YKIT CONFIGURING
+    YKit *launcher = [YKit getInstance];
+    
+    launcher.isPotrait = NO;
+    
+    //
+    // if using facebook API, you need to implement
+    // [launcher setPermissionFacebook:@"public_profile"]; // string is the permission you want to
+    //
+    [launcher setupWithWindow:window usingFacebookSDK:YES];
+    [launcher setPermissionFacebook:@"public_profile"];
+    
+    // Handle login callback
+    [launcher handleLoginWithCompletion:^(NSDictionary *data) {
+        [launcher getFacebookInfo];
+        NSString *userID = data[kParamUserID];
+        NSString *userName = data[kParamUserName];
+        NSString *accessToken = data[kParamAccessToken];
+        //  NSLog(@"sample %@" ,[launcher getFacebookInfo]);
+        [launcher showButtonLauncherWithAnimation:YES];
+    }];
+    
+    // Handle logout callback
+    [launcher handleLogoutWithCompletion:^{
+        //do something
+    }];
+    
+    // handle payment callback
+    [launcher handlePaymentWithCompletion:^(NSString *data){
+        NSLog(@"Payment success! %@", data);
+    }];
+    
+    //    NSLog(@"sample %@" ,[launcher getFacebookInfo]);
+    [launcher setDomainDebug:YES];
+    
+    if ([launcher silentLogin]) {
+        
+    }
+    // Example usage of silentLogin. For more information, check the public functions section.
+    //if([launcher silentLogin]){
+    //    // Move direct to game
+    //}
+    //else {
+    //    // Show login screen
+    //    [launcher showLoginScreen];
+    //}
+    
+    
+    // Example usage of setting up payment and then buy it. For more information, check section 2. Implementation payment info
+    //[launcher setPaymentInfo:@"Server ID" andCharId:@"Character ID" andPayment:@"Payment package ID"];
+    //[launcher buyItemWithGameOrder];
+    
+    
+    [launcher handleShowSDKCompletion:^{
+        NSLog(@"I'm in xD");
+    }];
+    
+    
+    NSString* appID = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+    [launcher handleCloseSDKCompletion:^{
+        NSLog(@"queee");
+    }];
+    
+    NSDictionary *dict = @{kParamApplication: ATNonNilObject(application),
+                           kParamOptions: ATNonNilObject(launchOptions)};
+    ATDispatchEvent(Event_AppDidFinishLaunching, dict);
+    // END YKIT CONFIGURING
+
+    // [START configure_firebase]
+    [FIRApp configure];
+    // [END configure_firebase]
+    
+    // [START set_messaging_delegate]
+    [FIRMessaging messaging].delegate = self;
+    // [END set_messaging_delegate]
+    
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+        // iOS 7.1 or earlier. Disable the deprecation warnings.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        UIRemoteNotificationType allNotificationTypes =
+        (UIRemoteNotificationTypeSound |
+         UIRemoteNotificationTypeAlert |
+         UIRemoteNotificationTypeBadge);
+        [application registerForRemoteNotificationTypes:allNotificationTypes];
+#pragma clang diagnostic pop
+    } else {
+        // iOS 8 or later
+        // [START register_for_notifications]
+        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+            UIUserNotificationType allNotificationTypes =
+            (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+            UIUserNotificationSettings *settings =
+            [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+            [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        } else {
+            // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+            // For iOS 10 display notification (sent via APNS)
+            [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+            UNAuthorizationOptions authOptions =
+            UNAuthorizationOptionAlert
+            | UNAuthorizationOptionSound
+            | UNAuthorizationOptionBadge;
+            [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            }];
+#endif
+        }
+        
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        // [END register_for_notifications]
+    }
+    
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)
+sourceApplication annotation:(id)annotation {
+    
+    NSDictionary *dict = @{kParamApplication: ATNonNilObject(application), kParamUrl: ATNonNilObject(url),
+                           kParamSourceApplication: ATNonNilObject(sourceApplication), kParamAnnotation: ATNonNilObject(annotation)};
+    
+    ATDispatchEvent(Event_AppOpenUrl, dict);
+    
+    return YES;
+}
+
+// [START receive_message]
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [[YKit getInstance] appDidReceiveMessage:userInfo];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    [[YKit getInstance] appDidReceiveMessage:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+// [END receive_message]
+
+// [START ios_10_message_handling]
+// Receive displayed notifications for iOS 10 devices.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// Handle incoming notification messages while app is in the foreground.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    NSDictionary *userInfo = notification.request.content.userInfo;
+    [[YKit getInstance] appDidReceiveMessage:userInfo];
+    // Change this to your preferred presentation option
+    completionHandler(UNNotificationPresentationOptionNone);
+}
+
+// Handle notification messages after display notification is tapped by the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void(^)())completionHandler {
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    [[YKit getInstance] appDidReceiveMessage:userInfo];
+    completionHandler();
+}
+#endif
+// [END ios_10_message_handling]
+
+// [START refresh_token]
+- (void)messaging:(nonnull FIRMessaging *)messaging didRefreshRegistrationToken:(nonnull NSString *)fcmToken {
+    [[YKit getInstance] setFCMToken:fcmToken];
+}
+// [END refresh_token]
+
+// [START ios_10_data_message]
+
+- (void)messaging:(FIRMessaging *)messaging didReceiveMessage:(FIRMessagingRemoteMessage *)remoteMessage {
+    [[YKit getInstance] appDidReceiveMessage:remoteMessage.appData];
+}
+// [END ios_10_data_message]
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Unable to register for remote notifications: %@", error);
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSString *fcmToken = [FIRMessaging messaging].FCMToken;
+    [[YKit getInstance] setFCMToken:fcmToken];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+    // We don't need to call this method any more. It will interrupt user defined game pause&resume logic
+    /* cocos2d::Director::getInstance()->pause(); */
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+    // We don't need to call this method any more. It will interrupt user defined game pause&resume logic
+    /* cocos2d::Director::getInstance()->resume(); */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+    cocos2d::Application::getInstance()->applicationDidEnterBackground();
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+    cocos2d::Application::getInstance()->applicationWillEnterForeground();
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+#if __has_feature(objc_arc)
+#else
+- (void)dealloc {
+    [window release];
+    [_viewController release];
+    [super dealloc];
+}
+#endif
+
+
+@end
